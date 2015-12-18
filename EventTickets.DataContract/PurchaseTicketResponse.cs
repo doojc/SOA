b9982d4814345d5c211a236904146f20923eaf87
DoojC <dujso.edin@gmail.com>
2015-12-18 18:43:08 +0100

    SOA Init

---
+++ EventTickets.DataContract/PurchaseTicketResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+
+namespace EventTickets.DataContract
+{
+    [DataContract]
+    public class PurchaseTicketResponse : Response
+    {
+        [DataMember]
+        public string TicketId { get; set; }
+
+        [DataMember]
+        public String EventName { get; set; }
+
+        [DataMember]
+        public String EventId { get; set; }
+
+        [DataMember]
+        public int NoOfTickets { get; set; }
+    }
+}
---
+++ EventTickets.DataContract/ReserveTicketResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+
+namespace EventTickets.DataContract
+{
+    [DataContract]
+    public class ReserveTicketResponse : Response
+    {
+        [DataMember]
+        public string ReservationNumber { get; set; }
+
+        [DataMember]
+        public DateTime ExpirationDate { get; set; }
+
+        [DataMember]
+        public String EventName { get; set; }
+
+        [DataMember]
+        public String EventId { get; set; }
+
+        [DataMember]
+        public int NoOfTickets { get; set; }
+    }
+}
